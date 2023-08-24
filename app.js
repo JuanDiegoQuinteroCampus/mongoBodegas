9f442ca304042c312b160a9e40bb8c7922647d55
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-08-24 08:21:59 -0500

    feat: :rocket: Implementation middleware and token
    Se le aplicó el dto y el token a las collection para mas seguridad en validación y manipulacion de la data

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import appBodegas from './routers/bodegas.js';
-/* import { appToken, appVerify } from './limit/token.js'; */
+import appProductos from './routers/productos.js';
+import { appToken, appVerify } from './limit/token.js';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-/* app.use("/token", appToken); */
+app.use("/token", appToken);
 
 
-app.use("/bodegas" ,appBodegas)
+app.use("/bodegas" ,appVerify,appBodegas);
+app.use("/productos" ,appVerify,appProductos)
+
 
 const config = JSON.parse(process.env.MY_SERVER);
 app.listen(config,()=>{
